8f354d6df80eeb947b649acfdd6ba8f445e8391e
edwinrodriguez <edwin_rodriguez_96@live.com>
2025-03-07 17:28:19 -0500

    Added comments to code.

--- ScreenRecorder.py
+++ ScreenRecorder.py
@@ -3,7 +3,7 @@ import cv2
 import numpy as np
 
 resolution = (1920, 1080)
-codec = cv2.VideoWriter_fourcc(*"MJPG")
+codec = cv2.VideoWriter_fourcc(*"MJPG") #MJPG is compatible with my mac.
 filename = "Recording.avi"
 fps = 30.0
 out = cv2.VideoWriter(filename, codec, fps, resolution)
@@ -14,11 +14,11 @@ cv2.resizeWindow("Live", 480, 270)
 while True:
     img = pyautogui.screenshot()
     frame = np.array(img)
-    frame = cv2.cvtColor(frame, cv2.COLOR_RGBA2BGR)
+    frame = cv2.cvtColor(frame, cv2.COLOR_RGBA2BGR) #RGBA2BGR is compatible with my mac.
     out.write(frame)
     cv2.imshow('Live', frame)
 
-    if cv2.waitKey(1) == ord('q'):
+    if cv2.waitKey(1) == ord('q'): #Pressing q quits the program.
         break
 
 out.release()
